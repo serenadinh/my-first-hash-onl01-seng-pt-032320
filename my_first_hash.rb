2f560a5be1e8412023095b6c8bed3cce0bf695a1
heyserenadinh Dinh <heyserenadinh@gmail.com>
2020-04-05 04:50:03 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,43 +4,20 @@ end
 
 
 def shipping_manifest
-  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil painting" =>3}
+  #set a variable called `shipping_manifest`, equal to a hash
+  #fill that hash with key/value pairs that describe the following information:
+  #We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
 
 def retrieval
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
   shipping_manifest["oil paintings"]
 end
 
 def adding
-  shipping_manifest = {
-                  "whale bone corsets" => 5,
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
-  # add 2 muskets to the shipping_manifest hash below
-  shipping_manifest = {
-                  "whale bone corsets" => 5,
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3,
-                  "muskets" => 2
-                 }
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3, "muskets" => 2, "gun powder" =>4}
+  
 
-
-  # add 4 gun powder to the shipping_manifest hash below
-   shipping_manifest = {
-                  "whale bone corsets" => 5,
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3,
-                  "muskets" => 2,
-                  "gun powder" => 4
-                 }
-
-
-  # return the shipping_manifest hash below
   shipping_manifest
 end
